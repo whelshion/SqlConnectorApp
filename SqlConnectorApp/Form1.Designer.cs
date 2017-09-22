ca9d3fd1eef0f81842b17adc6d7e8ecb868ec1af
Cyrus Zhou <cyrus.zhou@outlook.com>
2017-09-22 14:34:14 +0800

    添加项目文件。

---
+++ SqlConnectorApp/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace SqlConnectorApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.msgBox = new System.Windows.Forms.TextBox();
+            this.mySqlBtn = new System.Windows.Forms.Button();
+            this.msSqlBtn = new System.Windows.Forms.Button();
+            this.oracleBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // msgBox
+            // 
+            this.msgBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.msgBox.BackColor = System.Drawing.SystemColors.Info;
+            this.msgBox.ForeColor = System.Drawing.Color.Blue;
+            this.msgBox.Location = new System.Drawing.Point(13, 13);
+            this.msgBox.MaxLength = 999999999;
+            this.msgBox.Multiline = true;
+            this.msgBox.Name = "msgBox";
+            this.msgBox.ReadOnly = true;
+            this.msgBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.msgBox.Size = new System.Drawing.Size(1220, 510);
+            this.msgBox.TabIndex = 0;
+            // 
+            // mySqlBtn
+            // 
+            this.mySqlBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.mySqlBtn.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.mySqlBtn.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.mySqlBtn.Location = new System.Drawing.Point(374, 544);
+            this.mySqlBtn.Name = "mySqlBtn";
+            this.mySqlBtn.Size = new System.Drawing.Size(136, 34);
+            this.mySqlBtn.TabIndex = 1;
+            this.mySqlBtn.Text = "测试MySQL";
+            this.mySqlBtn.UseVisualStyleBackColor = false;
+            this.mySqlBtn.Click += new System.EventHandler(this.mySqlBtn_Click);
+            // 
+            // msSqlBtn
+            // 
+            this.msSqlBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.msSqlBtn.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.msSqlBtn.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.msSqlBtn.Location = new System.Drawing.Point(542, 544);
+            this.msSqlBtn.Name = "msSqlBtn";
+            this.msSqlBtn.Size = new System.Drawing.Size(136, 34);
+            this.msSqlBtn.TabIndex = 2;
+            this.msSqlBtn.Text = "测试MSSQL";
+            this.msSqlBtn.UseVisualStyleBackColor = false;
+            this.msSqlBtn.Click += new System.EventHandler(this.msSqlBtn_Click);
+            // 
+            // oracleBtn
+            // 
+            this.oracleBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.oracleBtn.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.oracleBtn.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.oracleBtn.Location = new System.Drawing.Point(709, 544);
+            this.oracleBtn.Name = "oracleBtn";
+            this.oracleBtn.Size = new System.Drawing.Size(136, 34);
+            this.oracleBtn.TabIndex = 3;
+            this.oracleBtn.Text = "测试Oracle";
+            this.oracleBtn.UseVisualStyleBackColor = false;
+            this.oracleBtn.Click += new System.EventHandler(this.oracleBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.clearBtn.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.clearBtn.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.clearBtn.Location = new System.Drawing.Point(873, 544);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(97, 34);
+            this.clearBtn.TabIndex = 4;
+            this.clearBtn.Text = "清空";
+            this.clearBtn.UseVisualStyleBackColor = false;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1245, 590);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.oracleBtn);
+            this.Controls.Add(this.msSqlBtn);
+            this.Controls.Add(this.mySqlBtn);
+            this.Controls.Add(this.msgBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(850, 500);
+            this.Name = "MainForm";
+            this.Text = "SQL连接测试工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox msgBox;
+        private System.Windows.Forms.Button mySqlBtn;
+        private System.Windows.Forms.Button msSqlBtn;
+        private System.Windows.Forms.Button oracleBtn;
+        private System.Windows.Forms.Button clearBtn;
+    }
+}
+
---
+++ SqlConnectorApp/Form1.cs
@@ -0,0 +1,273 @@
+﻿using MySql.Data.MySqlClient;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlConnectorApp
+{
+    public partial class MainForm : Form
+    {
+        private static string connStr = ConfigurationManager.AppSettings["mssqlconnstring"];
+        private static string queryStr = ConfigurationManager.AppSettings["querystring"];
+        private static string lineDelimiter = ConfigurationManager.AppSettings["linedelimiter"];
+        private string LineDelimiter
+        {
+            get
+            {
+                string tStr = string.IsNullOrWhiteSpace(lineDelimiter) ? "-" : lineDelimiter;
+                StringBuilder sb = new StringBuilder();
+                var pxWidth = this.msgBox.Width;
+                var loopTimes = pxWidth / 7 / (lineDelimiter?.Length ?? 1);
+                for (int i = 0; i < loopTimes; i++)
+                {
+                    sb.Append(lineDelimiter);
+                }
+                return sb.ToString();
+            }
+        }
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.msgBox.Text = string.Empty;
+            AppendMessage(DateTime.Now.ToLocalTime().ToString());
+            AppendMessage("  准备就绪...");
+            AppendMessageLine();
+        }
+
+        private void mySqlBtn_Click(object sender, EventArgs e)
+        {
+            connStr = ConfigurationManager.AppSettings["mysqlconnstring"];
+
+            AppendMessageLine(DateTime.Now.ToLocalTime().ToString());
+            AppendMessageLine("执行MySQL语句：" + queryStr);
+            AppendMessageLine(ExecuteMySqlQuery());
+            AppendMessageLine();
+            AppendMessageLine(LineDelimiter);
+        }
+
+
+        private void msSqlBtn_Click(object sender, EventArgs e)
+        {
+            connStr = ConfigurationManager.AppSettings["mssqlconnstring"];
+
+            AppendMessageLine();
+            AppendMessageLine(DateTime.Now.ToLocalTime().ToString());
+            AppendMessageLine("执行MSSQL语句：" + queryStr);
+            AppendMessageLine(ExecuteMsSqlQuery());
+            AppendMessageLine();
+            AppendMessageLine(LineDelimiter);
+
+        }
+
+        private void oracleBtn_Click(object sender, EventArgs e)
+        {
+            connStr = ConfigurationManager.AppSettings["orclconnstring"];
+
+            AppendMessageLine();
+            AppendMessageLine(DateTime.Now.ToLocalTime().ToString());
+            AppendMessageLine("执行Oracle语句：" + queryStr);
+            AppendMessageLine(ExecuteOracleQuery());
+            AppendMessageLine();
+            AppendMessageLine(LineDelimiter);
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            this.msgBox.Text = string.Empty;
+            AppendMessage(DateTime.Now.ToLocalTime().ToString());
+            AppendMessage("  准备就绪...");
+            AppendMessageLine();
+        }
+
+        private void AppendMessage(string message = "")
+        {
+            this.msgBox.AppendText(message);
+            this.msgBox.ScrollToCaret();
+        }
+        private void AppendMessageLine(string message = "")
+        {
+            this.msgBox.AppendText(Environment.NewLine + message);
+            this.msgBox.ScrollToCaret();
+        }
+
+
+        private string ExecuteMsSqlQuery()
+        {
+            string message = string.Empty;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter(queryStr, conn))
+                    {
+                        DataSet ds = new DataSet();
+                        sda.Fill(ds);
+                        if (ds.Tables.Count > 0)
+                        {
+                            message += DataTable2Sting(ds.Tables[0]);
+                        }
+                        else
+                        {
+                            message = "执行无结果";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message + Environment.NewLine + ex.StackTrace;
+            }
+            return message;
+        }
+
+        private string ExecuteMySqlQuery()
+        {
+            string message = string.Empty;
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    using (MySqlDataAdapter sda = new MySqlDataAdapter(queryStr, conn))
+                    {
+                        DataSet ds = new DataSet();
+                        sda.Fill(ds);
+                        if (ds.Tables.Count > 0)
+                        {
+                            message += DataTable2Sting(ds.Tables[0]);
+                        }
+                        else
+                        {
+                            message = "执行无结果";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message + Environment.NewLine + ex.StackTrace;
+            }
+            return message;
+        }
+
+        private string ExecuteOracleQuery()
+        {
+            string message = string.Empty;
+            try
+            {
+                using (OracleConnection conn = new OracleConnection(connStr))
+                {
+                    using (OracleDataAdapter sda = new OracleDataAdapter(queryStr, conn))
+                    {
+                        DataSet ds = new DataSet();
+                        sda.Fill(ds);
+                        if (ds.Tables.Count > 0)
+                        {
+                            message += DataTable2Sting(ds.Tables[0]);
+                        }
+                        else
+                        {
+                            message = "执行无结果";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message + Environment.NewLine + ex.StackTrace;
+            }
+            return message;
+        }
+
+
+        private string DataTable2Sting(DataTable dt)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                sb.Append("|");
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    sb.Append(dt.Columns[j].ColumnName).Append(":").Append(dt.Rows[i][j].ToString()).Append("|");
+                }
+                if (i == dt.Rows.Count - 1)
+                    continue;
+                sb.Append(Environment.NewLine);
+            }
+            return sb.ToString();
+        }
+
+
+
+
+
+        /// <summary>         
+        /// DataSetToList         
+        /// </summary>          
+        /// <typeparam name="T">转换类型</typeparam>         
+        /// <param name="dataSet">数据源</param>         
+        /// <param name="tableIndex">需要转换表的索引</param>        
+        /// /// <returns>泛型集合</returns>
+        public IList<T> DataSetToList<T>(DataSet dataset, int tableIndex = 0)
+        {
+            //确认参数有效
+            if (dataset == null || dataset.Tables.Count <= 0 || tableIndex < 0)
+            {
+                return null;
+            }
+            DataTable dt = dataset.Tables[tableIndex];
+            IList<T> list = new List<T>();
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                if (typeof(T) == typeof(int) || typeof(T) == typeof(string))
+                {
+                    list.Add((T)dt.Rows[0][0]);
+                }
+                else
+                {
+                    //创建泛型对象
+                    T _t = Activator.CreateInstance<T>();
+                    //获取对象所有属性
+                    PropertyInfo[] propertyInfo = _t.GetType().GetProperties();
+                    //属性和名称相同时则赋值
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        foreach (PropertyInfo info in propertyInfo)
+                        {
+                            if (dt.Columns[j].ColumnName.ToUpper().Equals(info.Name.ToUpper()))
+                            {
+                                if (dt.Rows[i][j] != DBNull.Value)
+                                {
+                                    info.SetValue(_t, dt.Rows[i][j], null);
+                                }
+                                else
+                                {
+                                    info.SetValue(_t, null, null);
+                                }
+                                break;
+                            }
+                        }
+                    }
+                    list.Add(_t);
+                }
+            }
+            return list;
+        }
+
+    }
+}
